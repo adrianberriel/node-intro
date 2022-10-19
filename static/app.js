a41df649d6d1d8f1e12b8d52edee830dd3ad69a8
Adrian Berriel <berriel@gmail.com>
2022-10-19 16:42:16 -0300

    Mocking GET routes

--- static/app.js
+++ static/app.js
@@ -1,9 +1,9 @@
 const API = 'http://localhost:3000'
 
-const populateProducts = async () => {
+const populateProducts = async (category) => {
   const products = document.querySelector('#products')
   products.innerHTML = ''
-  const res = await fetch(API)
+  const res = await fetch(`${API}/${category}`)
   const data = await res.json()
   for (const product of data) {
     const item = document.createElement('product-item')
@@ -16,9 +16,10 @@ const populateProducts = async () => {
     products.appendChild(item)
   }
 }
+const category = document.querySelector('#category')
 
-document.querySelector('#fetch').addEventListener('click', async () => {
-  await populateProducts()
+category.addEventListener('input', async ({ target }) => {
+  await populateProducts(target.value)
 })
 
 customElements.define('product-item', class Item extends HTMLElement {
